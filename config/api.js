44aff79e25aac1d107f4e3307a64c41b56dda0e8
Raveen Adhikari <148311365+raveenadhikari@users.noreply.github.com>
2025-10-15 11:27:52 +0000

    setting up databases

--- config/api.js
+++ config/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = '192.168.100.181'
+//const BASE_URL = '192.168.100.181'
 
-export const AUTH_URL = `http://${BASE_URL}:4000`
-export const EXPO_URL = `http://${BASE_URL}:5000`
+export const AUTH_URL = `https://bug-free-space-robot-7v9g6xx47v7v3v4w-15777.app.github.dev/`
+export const EXPO_URL = `https://bug-free-space-robot-7v9g6xx47v7v3v4w-14191.app.github.dev/`
